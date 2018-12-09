10bc935370625b4926961fd5cd54c252870ec6a0
Kazuya <shiozaki5621@gmail.com>
2018-12-09 00:20:43 -0800

    Finish Android fundamental Home work

--- android_fundamentals/AF02.2HomeWork/app/src/main/java/com/example/subarushiozaki/af022homework/MainActivity.java
+++ android_fundamentals/AF02.2HomeWork/app/src/main/java/com/example/subarushiozaki/af022homework/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.subarushiozaki.af022homework;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mCounterButton;
+    private TextView mCounter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCounterButton = findViewById(R.id.counterButton);
+
+
+        if (savedInstanceState != null) {
+            String tmp = savedInstanceState.getString("counter");
+            mCounter.setText(tmp);
+        } else {
+            mCounter = findViewById(R.id.counter);
+        }
+    }
+
+    public void incrimentCounter(View view) {
+        int count = Integer.valueOf(mCounter.getText().toString());
+        count++;
+        mCounter.setText(String.valueOf(count));
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("counter",mCounter.getText().toString());
     }
 }
